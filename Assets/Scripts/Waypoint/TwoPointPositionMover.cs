0a1a8ff789199c5f7f0c9a9d5d7fa765d3a0a83c
Jasminda Septianti Yuwono <jasminda.yuwono@gmail.com>
2026-02-28 20:28:31 +0700

    Add files via upload

---
+++ Assets/Scripts/Waypoint/TwoPointPositionMover.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoPointPositionMover : MonoBehaviour
+{
+    public Route2PointPosSO route;
+    public float speed = 2f;
+
+    private Vector3 target;
+
+    void Start()
+    {
+        transform.position = route.pointA;
+        target = route.pointB;
+    }
+
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position, target,
+            speed * Time.deltaTime
+        );
+
+        if (transform.position == target)
+        {
+            target = (target == route.pointA) ? route.pointB : route.pointA;
+        }
+    }
+}
+
